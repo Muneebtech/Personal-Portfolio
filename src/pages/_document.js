72eb74b23ac73799fb505c3269d923089c79d7e4
si-muneeb-tahir <muneeb.tahir@stackintel.io>
2023-01-07 14:04:12 +0500

    script tag 2

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -41,18 +41,7 @@ export default class MyDocument extends Document {
           <NextScript />
           <script
             type="text/javascript"
-            dangerouslySetInnerHTML={
-              (window._mfq =
-                window._mfq ||
-                [](function () {
-                  var mf = document.createElement("script");
-                  mf.type = "text/javascript";
-                  mf.defer = true;
-                  mf.src =
-                    "//cdn.mouseflow.com/projects/f5fd4855-9483-48f4-81e8-3cb8e988cbe7.js";
-                  document.getElementsByTagName("head")[0].appendChild(mf);
-                })())
-            }
+            src="../../src/entry_script"
           ></script>
         </body>
       </Html>
